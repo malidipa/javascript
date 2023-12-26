91e3eb3f8dc5f0443faf5ddfeae2ae090615407a
malidipa <malidipali2000@gmail.com>
2023-12-26 08:52:46 -0800

    My JS file
    My JS file

---
+++ 01_swapVariable.js
@@ -0,0 +1,31 @@
+var myFullName
+console.log("My Full Name is:",myFullName);
+ myFullName="Dipali mali";
+console.log("My Full Name is:",myFullName);
+var myFullName="Dipali Kashinath mali";
+console.log("My Full Name is:",myFullName);
+
+var mylove="l love only JS";
+console.log("My Love:",mylove);
+ var firstName="Dipali";
+ console.log("First Name:",firstName);
+ var lastName="Mali";
+ console.log("Last Name:",lastName);
+ var age="24";
+ console.log("Age:",age);
+ var address="Pune";
+ console.log("Address:",address);
+ var pinCode="411033";
+ console.log("PinCode:",pinCode);
+ var gender="femail";
+ console.log("Gender:",gender);
+ var  isMarried="No";
+ console.log("Is Married:",isMarried);
+
+ console.log=("first Name:",firstName);
+ console.log=("Last Name:",lastName);
+ console.log=("Age:",age)
+ console.log=("Address:",address)
+ console.log=("pincode:",pinCode)
+ console.log=("gender:",gender)
+ console.log=("Is Married:",isMarried)
