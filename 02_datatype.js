68efdaf024c28b1b5526540f88069afa108bf464
malidipa <malidipali2000@gmail.com>
2023-12-27 08:02:15 -0800

    data type JS file
    data type JS file

---
+++ 02_datatype.js
@@ -0,0 +1,90 @@
+console.log("=================================1===============================================")
+var bankName;
+console.log("Bank Nmae :",bankName,"           | "  ,"Data Type Of Operator:", typeof bankName);   
+var bankName = "SBI bank"
+console.log("Bank Nmae :",bankName,"            |  "  ,"Data Type Of Operator:", typeof bankName);
+console.log("=================================2===============================================")
+var mark="90";
+console.log("Marks:",mark,"                       | ","Data Type Of Operator:", typeof mark);
+console.log("=================================3================================================");
+var isWorking
+console.log("Before Initialization:",isWorking,"|","Data Type Of Operator:", typeof  isWorking );
+var isWorking=true;
+console.log("After Initialization:",isWorking,"      |","Data Type Of Operator:", typeof  isWorking);
+console.log("=================================4================================================");
+ var totalCount="Hundred and Seven";
+ console.log("Total Count:",totalCount,"  |","Data Type Of Operator:", typeof totalCount );
+ var  totalCount=107 
+ console.log("Total Count:",totalCount,"                |","Data Type Of Operator:", typeof totalCount );
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
